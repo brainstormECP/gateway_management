5d9ce791f8f168fde494292e8c0cdf1649414815
Elvis Jr Crego <elvis.crego@gmail.com>
2020-09-13 11:07:54 -0400

    Organizing projecs folders

--- src/GatewayManagement/Startup.cs
+++ src/GatewayManagement/Startup.cs
@@ -51,7 +51,7 @@ namespace gateway_management
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "../ClientApp/dist";
             });
         }
 
@@ -96,7 +96,7 @@ namespace gateway_management
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "../ClientApp";
 
                 if (env.IsDevelopment())
                 {
